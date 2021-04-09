65acd1cb269a99b8771a34836deca61faa5d642e
colineckert <colineckert10@gmail.com>
2021-04-08 18:31:37 -0700

    Create state consts

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { submitQuestion } from '../actions/question_actions';
 
 const Form = ({ submitQuestion }) => {
-  
+  const [name, setName] = useState('');
+  const [text, setText] = useState('');
+  const [repo, setRepo] = useState('');
+  const [live, setLive] = useState('');
+
   return (
     <>
-    
+      <input type="text" placeholder="name" onChange={() => setName()}>{name}</input>
+      <input type="text" placeholder="name" onChange={() => setText()}>{text}</input>
+      <input type="text" placeholder="name" onChange={() => setRepo()}>{repo}</input>
+      <input type="text" placeholder="name" onChange={() => setLive()}>{live}</input>
     </>
   )
 }
